2525aa1d38c5998e5bf8cf45b312518a8ee9bb5f
Felipe Garcia <felipe_garcia99@hotmail.com>
2021-07-21 20:16:05 -0300

    feat: lighttpd build preparation

---
+++ cap15/prepare.sh
@@ -0,0 +1,19 @@
+# Download dos códigos-fonte
+cd ~/Downloads/
+
+# pcre
+wget https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.gz
+# lighttpd
+wget https://download.lighttpd.net/lighttpd/releases-1.4.x/lighttpd-1.4.59.tar.gz
+
+# Extração
+tar -xf pcre-8.44.tar.gz
+tar -xf lighttpd-1.4.59.tar.gz
+
+# Movendo os códigos-fonte para a pasta “board-support”
+mv pcre-8.44.tar.gz ~/texasSDK/board-support/
+mv lighttpd-1.4.59.tar.gz ~/texasSDK/board-support/
+
+# Clonando apenas a pasta lighttpd
+cd ~/texasSDK/board-support/
+cp lighttpd-1.4.59.tar.gz lighttpd-1.4.59-RASCUNHO.tar.gz
