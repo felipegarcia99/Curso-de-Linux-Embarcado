8c4d8610e5023c144dc2e033351d668092391abf
Felipe Garcia <felipe_garcia99@hotmail.com>
2021-07-22 00:55:10 -0300

    fix: repare typo in cp command

--- cap15/prepare.sh
+++ cap15/prepare.sh
@@ -16,4 +16,4 @@ mv lighttpd-1.4.59.tar.gz ~/texasSDK/board-support/
 
 # Clonando apenas a pasta lighttpd
 cd ~/texasSDK/board-support/
-cp lighttpd-1.4.59.tar.gz lighttpd-1.4.59-RASCUNHO.tar.gz
+cp lighttpd-1.4.59/ lighttpd-1.4.59-RASCUNHO/
