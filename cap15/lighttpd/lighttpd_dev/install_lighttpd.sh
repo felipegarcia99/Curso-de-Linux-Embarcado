83abb58341723f5b11e9193cf28ecf3bd9d2c1ef
Felipe Garcia <felipe_garcia99@hotmail.com>
2021-07-22 00:57:08 -0300

    docs: add additional information to the comment

--- cap15/lighttpd/lighttpd_dev/install_lighttpd.sh
+++ cap15/lighttpd/lighttpd_dev/install_lighttpd.sh
@@ -5,7 +5,7 @@ useradd -g lighttpd -d /var/www/html -s /sbin/nologin lighttpd
 # Alterar o dono do arquivo e o grupo
 chown lighttpd:root /etc/lighttpd/lighttpd.conf
 
-# habilitando o lighttpd na inicialização (NÃO FUNCIONA por causa do comando daemon)
+# habilitando o lighttpd na inicialização (NÃO FUNCIONA por causa da falta do comando daemon no target)
 # systemctl enable lighttpd
 # systemctl start lighttpd
 # systemctl status lighttpd
