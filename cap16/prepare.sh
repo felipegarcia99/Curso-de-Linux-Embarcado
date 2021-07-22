c9730735ebc77ee4ea687b11be40c28e4fcd2498
Felipe Garcia <felipe_garcia99@hotmail.com>
2021-07-22 01:15:35 -0300

    feat: preparation for application development

---
+++ cap16/prepare.sh
@@ -0,0 +1,10 @@
+# Criação do diretório do projeto
+cd ~/texasSDK
+mkdir codigos
+mkdir codigos/aplicacao_final_mysql_BBB
+
+# Criação dos arquivos essenciais
+cd ~/texasSDK/codigos/aplicacao_final_mysql_BBB
+mkdir build
+touch main.cpp
+touch CMakeLists.txt
